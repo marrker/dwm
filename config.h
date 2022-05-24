128956aba0153e7759144373771037a513c84670
marrker <edkbgoldfish05@gmail.com>
2022-05-24 14:59:50 -0400

    third commit

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 2; /* border pixel size of windows */
-static const unsigned int gappx = 0;    /* gaps size between windows */
+static const unsigned int borderpx = 3; /* border pixel size of windows */
+static const unsigned int gappx = 23;   /* gaps size between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int systrayspacing = 2;
 static const int showbar = 1; /* 0 means no bar */
@@ -11,15 +11,15 @@ static const char *fonts[] = {"JetBrainsMono Nerd Font:size=12",
                               "fontawesome:size=12"};
 static const char dmenufont[] = "JetBrainsMono Nerd Font:size=11";
 // background color
-static const char col_gray1[] = "#1a1b26";
+static const char col_gray1[] = "#1f2227";
 // inactive window border color
-static const char col_gray2[] = "#444444";
+static const char col_gray2[] = "#abb2bf";
 // font color
 static const char col_gray3[] = "#bbbbbb";
 // current tag and current window font color
 static const char col_gray4[] = "#eeeeee";
 // Top bar second color (blue) and active window border color
-static const char col_cyan[] = "#7aa2d7";
+static const char col_cyan[] = "#81a1c1";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
@@ -106,6 +106,8 @@ static Key keys[] = {
     {MODKEY, XK_d, spawn, {.v = dmenucmd}},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_t, togglebar, {0}},
+    {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
+    {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
     {MODKEY, XK_i, incnmaster, {.i = +1}},
@@ -114,10 +116,11 @@ static Key keys[] = {
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
     {MODKEY, XK_z, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
+    {MODKEY | ShiftMask, XK_Return, zoom, {0}},
     {MODKEY, XK_q, killclient, {0}},
     {MODKEY | ShiftMask, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY | ShiftMask, XK_m, setlayout, {.v = &layouts[2]}},
+    {MODKEY, XK_f, setlayout, {.v = &layouts[2]}},
     {MODKEY | ShiftMask, XK_space, setlayout, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
     {MODKEY, XK_0, view, {.ui = ~0}},
@@ -131,6 +134,15 @@ static Key keys[] = {
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     {MODKEY, XK_n, shiftview, {.i = +1}},
     {MODKEY, XK_b, shiftview, {.i = -1}},
+    {MODKEY, XK_comma, focusmon, {.i = -1}},
+    {MODKEY, XK_period, focusmon, {.i = +1}},
+    {MODKEY, XK_g, setgaps, {0}},
+    {MODKEY | ShiftMask, XK_g, setgaps, {23}},
+    {MODKEY | ShiftMask, XK_b, spawn,
+     SHCMD("feh --bg-fill --randomize ~/Pictures/wallpapers/* &")},
+    {MODKEY, XK_w, spawn, SHCMD("firefox")},
+    {MODKEY, XK_e, spawn, SHCMD("thunar")},
+    {MODKEY, XK_r, spawn, SHCMD("st -e ranger")},
     /*{ MODKEY,                       XK_F8,     spawn,          {.v = upvol   }
        }, */
     /*{ MODKEY,                       XK_F7,     spawn,          {.v = downvol }
